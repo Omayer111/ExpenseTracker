24b5a7f998ae524d0987934910ee64bd107c597f
“Polok004” <polok2007004@stud.kuet.ac.bd>
2024-04-03 15:24:22 +0600

    New Main Layout Created

---
+++ ExpenseTracker-master/app/src/main/java/com/example/expensetracker/views/activities/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.expensetracker.views.activities;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.expensetracker.R;
+import com.example.expensetracker.databinding.ActivityMainBinding;
+import com.example.expensetracker.views.fragments.Add_Transaction_Fragment;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+   ActivityMainBinding binding;
+   Calendar calendar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        setSupportActionBar(binding.materialToolbar2);
+        getSupportActionBar().setTitle("Transaction");
+        calendar=Calendar.getInstance();
+        updateDate();
+
+        binding.nextDateBtn.setOnClickListener(c->{
+            calendar.add(Calendar.DATE,1);
+            updateDate();
+        });
+
+        binding.PreviousDateBtn.setOnClickListener(c->{
+            calendar.add(Calendar.DATE,-1);
+            updateDate();
+        });
+
+        binding.floatingActionButton.setOnClickListener(c ->{
+            new Add_Transaction_Fragment().show(getSupportFragmentManager(),null);
+        });
+
+    }
+
+    void updateDate(){
+        SimpleDateFormat dateFormat=new SimpleDateFormat("dd MMMM,YYYY");
+        binding.currentDate.setText(dateFormat.format(calendar.getTime()));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
